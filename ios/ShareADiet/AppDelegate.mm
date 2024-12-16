2c6eb1617dea57bcbb87a5480fe4b9c8afacb097
Hussain Saifuddin <hussainquetta53@gmail.com>
2024-12-17 00:59:34 +0500

    location and map config

--- ios/ShareADiet/AppDelegate.mm
+++ ios/ShareADiet/AppDelegate.mm
@@ -7,6 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[GMSServices provideAPIKey:@"AIzaSyBNIx8hF5fYoN3AXEYkhKLKyWU9GrCFXkU"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"main";
 
   // You can add your custom initial props in the dictionary below.
