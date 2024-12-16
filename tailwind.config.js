e77f1ae2ed5590538758dead0cc580f4f59ffb64
Hussain Saifuddin <hussainquetta53@gmail.com>
2024-12-16 15:29:36 +0500

    install all necessary libs

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      "body-sm": "12px",
+      "body-md": "14px",
+      "body-lg": "16px",
+      "label-sm": "12px",
+      "label-md": "14px",
+      "label-lg": "16px",
+      "title-sm": "18px",
+      "title-md": "20px",
+      "title-lg": "22px",
+      "head-xs": "18px",
+      "head-sm": "24px",
+      "head-md": "28px",
+      "head-lg": "40px",
+      "head-xl": "48px",
+    },
+    extend: {
+      colors: {
+        warning: "#FEDF89",
+        errorText: "#e11900",
+        primary: '#323150',
+        secondary50: "#FFCFC6",
+        secondary100: '#FFEFED',
+        secondary200: "#F36A26",
+        inputBgColor: "#1F2A370D"
+      },
+      borderRadius: {
+        lg: "0.5rem",
+        md: "0.4rem",
+        sm: "0.3rem",
+      },
+    },
+  },
+  plugins: [],
+};
